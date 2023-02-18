6895accfc75d04e88057e5e33e63b7d4cdfc80ac
mrivs <valentinilyins@gmail.com>
2023-02-18 23:01:34 +0300

    hw2 task 3 added

--- homework02/Task23.java
+++ homework02/Task23.java
@@ -7,8 +7,43 @@
 // Студент Краснов получил 5 по предмету Физика.
 package homework02;
 
-public class Task23 {public static void main(String[] args) {
-    
-}
-    
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Task23 {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        String str = readerFile("homework02/task23.txt");
+        String[] arr = convert(str);
+        for (String s : arr) {
+            System.out.println(s);
+        }
+
+    }
+
+    private static String[] convert(String str) {
+        str = str.substring(2, str.length() - 1);
+        str = str.replaceAll("\"", "");
+        str = str.replaceAll("\\}", ".");
+        str = str.replaceAll("[,:]", " ");
+        str = str.replaceAll("фамилия", "Студент");
+        str = str.replaceAll("оценка", "получил");
+        str = str.replaceAll("предмет", "по предмету");
+        String[] arr = str.split("\\{");
+        return arr;
+    }
+
+    private static String readerFile(String pathFile) throws FileNotFoundException, IOException {
+
+        String str = "empty";
+        try (BufferedReader br = new BufferedReader(new FileReader(pathFile))) {
+            StringBuilder sb = new StringBuilder();
+            str = br.readLine();
+        }
+        return str;
+    }
+
 }
