a33675acd0809bc6f158fd239b9db5ba0890b354
mrivs <valentinilyins@gmail.com>
2023-02-19 21:06:48 +0300

    hw2 task 2 added

--- homework02/Task22.java
+++ homework02/Task22.java
@@ -1,9 +1,47 @@
-// Реализуйте алгоритм сортировки пузырьком числового массива, результат после каждой итерации запишите в лог-файл.
+// Реализуйте алгоритм сортировки пузырьком числового массива,
+// результат после каждой итерации запишите в лог-файл.
 
 package homework02;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
 public class Task22 {
-    public static void main(String[] args) {
-        
+
+    private static Logger lg = Logger.getLogger(Task22.class.getName());
+
+    public static void main(String[] args) throws SecurityException, IOException {
+
+        FileHandler fh = new FileHandler("homework02/task22log.txt");
+        lg.addHandler(fh);
+        SimpleFormatter sFormat = new SimpleFormatter();
+        fh.setFormatter(sFormat);
+
+        int[] arr = new int[] { 2, 9, 3, 4, 6, 7, 1, 5, 10, 8 };
+        bubsort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void bubsort(int[] arr) {
+        int temp;
+        boolean sorted = true;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    sorted = false;
+                    lg.info(Arrays.toString(arr));
+                }
+            }
+            if (sorted == true)
+                return;
+            else
+                sorted = true;
+        }
     }
 }
--- homework02/Task23.java
+++ homework02/Task23.java
@@ -11,8 +11,6 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.util.Arrays;
 
 public class Task23 {
     public static void main(String[] args) throws FileNotFoundException, IOException {
