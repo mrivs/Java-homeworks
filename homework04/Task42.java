6f1f7f5bbe5dc806fe3ac5e1d83e68c4a657eb5b
mrivs <valentinilyins@gmail.com>
2023-02-27 21:32:49 +0300

    hw4 t2 added

--- homework04/Task42.java
+++ homework04/Task42.java
@@ -1,10 +1,36 @@
 package homework04;
 
+import java.util.LinkedList;
+
+
 // Реализуйте очередь с помощью LinkedList со следующими методами:
-// enqueue() - помещает элемент в конец очереди, dequeue() - возвращает первый элемент из очереди и удаляет его, first() - возвращает первый элемент из очереди, не удаляя.
+// enqueue() - помещает элемент в конец очереди,
+//  dequeue() - возвращает первый элемент из очереди и удаляет его,
+//   first() - возвращает первый элемент из очереди, не удаляя.
 
 public class Task42 {
     public static void main(String[] args) {
-        
+        LinkedList<Object> ll = new LinkedList<>();
+
+        enqueue(ll, "one");
+        enqueue(ll, 2);
+        enqueue(ll, "three");
+        enqueue(ll, 4);
+        System.out.println(ll);
+        System.out.println(first(ll));
+        System.out.println(dequeue(ll));
+        System.out.println(ll);
+    }
+
+    public static void enqueue(LinkedList<Object> ll, Object e) {
+        ll.addLast(e);
+    }
+
+    public static Object dequeue(LinkedList<Object> ll) {
+        return ll.removeFirst();
+    }
+
+    private static Object first(LinkedList<Object> ll) {
+        return ll.getFirst();
     }
 }
